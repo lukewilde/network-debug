d7b0cb892b65a0c56d1c5c8a819056b94b34642c
wilde <luke.wilde@gmail.com>
2023-06-07 15:27:01 +0100

    Only logging legit packet loss

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const target = '8.8.8.8';
 
 // The number of pings to send for each run of MTR
-const cycles = 10;
+const cycles = 1;
 
 // The name of the file to write the output to
 const outputFile = 'mtr_report.txt';
@@ -17,14 +17,25 @@ function runMtr() {
             return;
         }
 
-        // Append the output to the file
-        fs.appendFile(outputFile, stdout, (err) => {
-            if (err) throw err;
-            console.log('MTR output saved!');
+        // Parse the output and check for packet loss
+        const lines = stdout.split('\n');
+        for (let line of lines) {
+            const fields = line.split(/\s+/);
+            const host = fields[2];
+            const loss = parseFloat(fields[3]);
+            console.log(host, loss)
+            if (host !== '???' && loss > 10) {
+                // Append the output to the file
+                fs.appendFile(outputFile, stdout, (err) => {
+                    if (err) throw err;
+                    console.log('MTR output saved!');
+                });
+                break;
+            }
+        }
 
-            // Start the next run of MTR
-            runMtr();
-        });
+        // Start the next run of MTR
+        runMtr();
     });
 }
 
